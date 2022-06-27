7926db096b9244f6a98ce81e718800ccfed89006
ashish <asheeshh@duck.com>
2022-06-27 09:45:05 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import blog from "https://deno.land/x/blog/blog.tsx";
+
+blog({
+    author: "ashish",
+    title: "",
+    description: "just a place to dump my occasional thoughts.",
+    avatar: "https://avatars.githubusercontent.com/u/68690233?v=4",
+    avatarClass: "rounded-full",
+    links: [
+      { title: "email", url: "mailto:asheeshh@duck.com" },
+      { title: "github", url: "https://github.com/asheeeshh" },
+      { title: "twitter", url: "https://twitter.com/_asheeshh" },
+      { title: "instagram", url: "https://www.instagram.com/_asheeshh/" },
+    ],
+    lang: "en",
+    timezone: "en-US",
+  });
---
+++ posts/what.md
@@ -0,0 +1,10 @@
+---
+title: what's this?
+publish_date: 2022-06-27
+---
+
+this place can be thought of as a public journal, a place where i would be posting my random thoughts (which can be very weird sometimes lol). i won't be posting anything serious here, but i will be posting random things that i think are interesting or things happening in my life, my views on different things and anything between. 
+
+i won't post anything regularly here as I'm very bad at maintaining a jornal, but I'll try to post something every now and then.
+
+thanks for reading <3
